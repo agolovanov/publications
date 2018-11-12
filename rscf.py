d5b695db6e730b62426b2eb62c6fdd2799e8094b
Anton Golovanov <agolovanov256@gmail.com>
2018-11-12 15:36:45 +0300

    Add 3 publications; update journals

--- rscf.py
+++ rscf.py
@@ -6,7 +6,7 @@ import database as pdb
 db = pdb.Database(languages=sys.argv[1], types='Article', format='unicode', with_page_prefix=True)
 db.et_al = 'et al'
 db.et_al_ru = 'и др'
-db.authors(max_authors=3)
+#db.authors(max_authors=3)
 table = db.get({'Paper': '$a. $t. $i', 'URL': 'URL: $URL', 'DOI': 'DOI: $DOI', 'IF': 'IF: $IF'})
 
 for _, row in table.iterrows():
